f1b91541997b5584a48e03d9d1e921c77f1dce71
Carlo Caione <carlo@endlessm.com>
2016-11-04 20:31:59 +0100

    s905x: Rewrite configuration file
    s905x.h made my head hurts. Rewrite / refactor / rework it.
    
    Signed-off-by: Carlo Caione <carlo@endlessm.com>

--- include/configs/s905x.h
+++ include/configs/s905x.h
@@ -1,8 +1,7 @@
-
 /*
- * board/amlogic/configs/gxl_p212_v1.h
+ * include/configs/s905x.h
  *
- * Copyright (C) 2015 Amlogic, Inc. All rights reserved.
+ * Copyright (C) 2016 Endless Computers
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,304 +18,277 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#ifndef __GXL_P212_V1_H__
-#define __GXL_P212_V1_H__
+#ifndef __S905X_H__
+#define __S905X_H__
 
 #ifndef __SUSPEND_FIRMWARE__
 #include <asm/arch/cpu.h>
-#endif		/* for compile problem of A53 and m3 */
+#endif
 
-#define CONFIG_SYS_GENERIC_BOARD  1
+#define CONFIG_SYS_GENERIC_BOARD
 #ifndef __SUSPEND_FIRMWARE__
 #ifndef CONFIG_AML_MESON
 #warning "include warning"
 #endif
-#endif		/* for compile problem of A53 and m3 */
+#endif
 
-/*
- * platform power init config
- */
+/* platform power init config */
 #define CONFIG_PLATFORM_POWER_INIT
-#define CONFIG_VCCK_INIT_VOLTAGE	1120
-#define CONFIG_VDDEE_INIT_VOLTAGE	1000		// voltage for power up
-#define CONFIG_VDDEE_SLEEP_VOLTAGE	 850		// voltage for suspend
+#define CONFIG_VCCK_INIT_VOLTAGE		1120
+#define CONFIG_VDDEE_INIT_VOLTAGE		1000
+#define CONFIG_VDDEE_SLEEP_VOLTAGE		850
 
 /* configs for CEC */
-#define CONFIG_CEC_OSD_NAME		"Mbox"
+#define CONFIG_CEC_OSD_NAME			"Mbox"
 #define CONFIG_CEC_WAKEUP
 
 #define CONFIG_INSTABOOT
 
 /* SMP Definitinos */
-#define CPU_RELEASE_ADDR		secondary_boot_func
+#define CPU_RELEASE_ADDR			secondary_boot_func
 
 /* Serial config */
-#define CONFIG_CONS_INDEX 2
-#define CONFIG_BAUDRATE  115200
-#define CONFIG_AML_MESON_SERIAL   1
-#define CONFIG_SERIAL_MULTI		1
+#define CONFIG_CONS_INDEX			2
+#define CONFIG_BAUDRATE				115200
+#define CONFIG_AML_MESON_SERIAL
+#define CONFIG_SERIAL_MULTI			1
 
-//Enable ir remote wake up for bl30
-//#define CONFIG_IR_REMOTE
-//#define CONFIG_AML_IRDETECT_EARLY
-#define CONFIG_IR_REMOTE_POWER_UP_KEY_CNT 4
-#define CONFIG_IR_REMOTE_USE_PROTOCOL 0         // 0:nec  1:duokan  2:Toshiba 3:rca 4:rcmm
-#define CONFIG_IR_REMOTE_POWER_UP_KEY_VAL1 0XE51AFB04 //amlogic tv ir --- power
-#define CONFIG_IR_REMOTE_POWER_UP_KEY_VAL2 0XBB44FB04 //amlogic tv ir --- ch+
-#define CONFIG_IR_REMOTE_POWER_UP_KEY_VAL3 0xF20DFE01 //amlogic tv ir --- ch-
-#define CONFIG_IR_REMOTE_POWER_UP_KEY_VAL4 0xBA45BD02
+/* IR */
+#define CONFIG_IR_REMOTE_POWER_UP_KEY_CNT	4
+#define CONFIG_IR_REMOTE_USE_PROTOCOL		0
+#define CONFIG_IR_REMOTE_POWER_UP_KEY_VAL1	0XE51AFB04
+#define CONFIG_IR_REMOTE_POWER_UP_KEY_VAL2	0XBB44FB04
+#define CONFIG_IR_REMOTE_POWER_UP_KEY_VAL3	0xF20DFE01
+#define CONFIG_IR_REMOTE_POWER_UP_KEY_VAL4	0xBA45BD02
 
-#define CONFIG_IR_REMOTE_POWER_UP_KEY_VAL5 0x3ac5bd02
-/* args/envs */
-#define CONFIG_SYS_MAXARGS  64
+#define CONFIG_IR_REMOTE_POWER_UP_KEY_VAL5	0x3ac5bd02
+
+/* Environment */
+#define CONFIG_SYS_MAXARGS			64
 #define CONFIG_EXTRA_ENV_SETTINGS \
-        "fdt_high=0x20000000\0"\
-        "testfile=/boot/uEnv.txt\0"\
-        "loadaddr=0x11000000\0"\
-        "initrd_loadaddr=0x13000000\0"\
-        "script_loadaddr=0x11000000\0"\
-        "get_rootuuid="\
-            "fsuuid mmc ${bootdev}:1 rootuuid"\
-            "\0"\
-        "load_boot_env="\
-            "echo >>> Loading Boot Script <<<; "\
-            "ext4load mmc ${bootdev}:1 ${script_loadaddr} /boot/uEnv.txt"\
-            "\0"\
-        "import_boot_env="\
-            "echo >>> Importing environment <<<; "\
-            "env import -t ${script_loadaddr} ${filesize}"\
-            "\0"\
-        "uenv_bootcmd="\
-            "echo >>> Booting <<<; "\
-            "ext4load mmc ${bootdev}:1 ${loadaddr} /boot/${kernel_image}; "\
-            "ext4load mmc ${bootdev}:1 ${initrd_loadaddr} /boot/${ramdisk_image}; "\
-            "bootm ${loadaddr} ${initrd_loadaddr}"\
-            "\0"\
-        "endless_select_bootdev="\
-            "if test -e mmc 0:1 ${testfile}; then "\
-                "setenv bootdev 0; "\
-            "else if test -e mmc 1:1 ${testfile}; then "\
-                "setenv bootdev 1; "\
-            "fi;fi;"\
-            "run endless_boot"\
-            "\0"\
-        "endless_boot="\
-            "run load_boot_env; "\
-            "run import_boot_env; "\
-            "run get_rootuuid; "\
-            "setenv bootargs root=UUID=${rootuuid} console=ttyS0,115200 ${bootargs}; "\
-            "run uenv_bootcmd"\
-            "\0"\
+	"fdt_high=0x20000000\0"\
+	"testfile=/boot/uEnv.txt\0"\
+	"loadaddr=0x11000000\0"\
+	"initrd_loadaddr=0x13000000\0"\
+	"script_loadaddr=0x11000000\0"\
+	"get_rootuuid="\
+		"fsuuid mmc ${bootdev}:1 rootuuid"\
+		"\0"\
+	"load_boot_env="\
+		"echo >>> Loading Boot Script <<<; "\
+		"ext4load mmc ${bootdev}:1 ${script_loadaddr} /boot/uEnv.txt"\
+		"\0"\
+	"import_boot_env="\
+		"echo >>> Importing environment <<<; "\
+		"env import -t ${script_loadaddr} ${filesize}"\
+		"\0"\
+	"uenv_bootcmd="\
+		"echo >>> Booting <<<; "\
+		"ext4load mmc ${bootdev}:1 ${loadaddr} /boot/${kernel_image}; "\
+		"ext4load mmc ${bootdev}:1 ${initrd_loadaddr} /boot/${ramdisk_image}; "\
+		"bootm ${loadaddr} ${initrd_loadaddr}"\
+		"\0"\
+	"endless_select_bootdev="\
+		"if test -e mmc 0:1 ${testfile}; then "\
+			"setenv bootdev 0; "\
+		"else if test -e mmc 1:1 ${testfile}; then "\
+			"setenv bootdev 1; "\
+		"fi;fi;"\
+		"run endless_boot"\
+		"\0"\
+	"endless_boot="\
+		"run load_boot_env; "\
+		"run import_boot_env; "\
+		"run get_rootuuid; "\
+		"setenv bootargs root=UUID=${rootuuid} console=ttyS0,115200 ${bootargs}; "\
+		"run uenv_bootcmd"\
+		"\0"\
 
-#define CONFIG_PREBOOT "store init"
-#define CONFIG_BOOTCOMMAND "run endless_select_bootdev"
+#define CONFIG_PREBOOT				"store init"
+#define CONFIG_BOOTCOMMAND			"run endless_select_bootdev"
 
-#define CONFIG_ENV_IS_NOWHERE  1
-#define CONFIG_ENV_SIZE   (64*1024)
-#define CONFIG_FIT 1
-#define CONFIG_OF_LIBFDT 1
-#define CONFIG_ANDROID_BOOT_IMAGE 1
-#define CONFIG_ANDROID_IMG 1
-#define CONFIG_SYS_BOOTM_LEN (64<<20) /* Increase max gunzip size*/
+#define CONFIG_ENV_IS_NOWHERE
+#define CONFIG_ENV_SIZE				(64 * 1024)
+#define CONFIG_ENV_OVERWRITE
+#define CONFIG_CMD_SAVEENV
 
-/* cpu */
-#define CONFIG_CPU_CLK					1200 //MHz. Range: 600-1800, should be multiple of 24
+/* Image setup */
+#define CONFIG_FIT
+#define CONFIG_OF_LIBFDT
+#define CONFIG_ANDROID_BOOT_IMAGE
+#define CONFIG_ANDROID_IMG
+#define CONFIG_SYS_BOOTM_LEN			(64 << 20)
 
-/* ddr */
-#define CONFIG_DDR_SIZE					0 //MB //0 means ddr size auto-detect
-#define CONFIG_DDR_CLK					912  //MHz, Range: 384-1200, should be multiple of 24
-#define CONFIG_DDR4_CLK					1100  //MHz, for boards which use different ddr chip
+/* CPU */
+#define CONFIG_CPU_CLK				1200	/* MHz. Range: 600-1800, should be multiple of 24 */
+
+/* DDR */
+#define CONFIG_DDR_SIZE				0	/* MB // 0 means ddr size auto-detect */
+#define CONFIG_DDR_CLK				912	/* MHz, Range: 384-1200, should be multiple of 24 */
+#define CONFIG_DDR4_CLK				1100	/* MHz, for boards which use different ddr chip */
+
 /* DDR type setting
  *    CONFIG_DDR_TYPE_LPDDR3   : LPDDR3
  *    CONFIG_DDR_TYPE_DDR3     : DDR3
  *    CONFIG_DDR_TYPE_DDR4     : DDR4
  *    CONFIG_DDR_TYPE_AUTO     : DDR3/DDR4 auto detect */
-#define CONFIG_DDR_TYPE					CONFIG_DDR_TYPE_DDR3
+#define CONFIG_DDR_TYPE				CONFIG_DDR_TYPE_DDR3
+
 /* DDR channel setting, please refer hardware design.
  *    CONFIG_DDR0_RANK0        : DDR0 rank0
  *    CONFIG_DDR0_RANK01       : DDR0 rank0+1
  *    CONFIG_DDR0_16BIT        : DDR0 16bit mode
  *    CONFIG_DDR_CHL_AUTO      : auto detect RANK0 / RANK0+1 */
 #define CONFIG_DDR_CHANNEL_SET			CONFIG_DDR_CHL_AUTO
-#define CONFIG_DDR_FULL_TEST			0 //1 for ddr full test
-#define CONFIG_NR_DRAM_BANKS			1
-/* ddr functions */
-#define CONFIG_CMD_DDR_D2PLL			0 //0:disable, 1:enable. d2pll cmd
-#define CONFIG_CMD_DDR_TEST				0 //0:disable, 1:enable. ddrtest cmd
-#define CONFIG_DDR_LOW_POWER			0 //0:disable, 1:enable. ddr clk gate for lp
-#define CONFIG_DDR_ZQ_PD				0 //0:disable, 1:enable. ddr zq power down
-#define CONFIG_DDR_USE_EXT_VREF			0 //0:disable, 1:enable. ddr use external vref
 
-/* storage: emmc/nand/sd */
-#define	CONFIG_STORE_COMPATIBLE 1
-//#define CONFIG_AML_NAND	1
-/* env */
-#define 	CONFIG_ENV_OVERWRITE
-#define 	CONFIG_CMD_SAVEENV
-/* fixme, need fix*/
+#define CONFIG_DDR_FULL_TEST			0	/* 1 for ddr full test */
+#define CONFIG_NR_DRAM_BANKS			1
+
+/* DDR functions */
+#define CONFIG_CMD_DDR_D2PLL			0	/* 0:disable, 1:enable. d2pll cmd */
+#define CONFIG_CMD_DDR_TEST			0	/* 0:disable, 1:enable. ddrtest cmd */
+#define CONFIG_DDR_LOW_POWER			0	/* 0:disable, 1:enable. ddr clk gate for lp */
+#define CONFIG_DDR_ZQ_PD			0	/* 0:disable, 1:enable. ddr zq power down */
+#define CONFIG_DDR_USE_EXT_VREF			0	/* 0:disable, 1:enable. ddr use external vref */
+
+/* storage: eMMC/NAND/SD */
+#define	CONFIG_STORE_COMPATIBLE
 
 #if (defined(CONFIG_ENV_IS_IN_AMLNAND) || defined(CONFIG_ENV_IS_IN_MMC)) && defined(CONFIG_STORE_COMPATIBLE)
 #error env in amlnand/mmc already be compatible;
 #endif
-#define CONFIG_AML_SD_EMMC 1
-#ifdef	CONFIG_AML_SD_EMMC
-	#define CONFIG_GENERIC_MMC 1
-	#define CONFIG_CMD_MMC 1
-	#define	CONFIG_SYS_MMC_ENV_DEV 1
-	#define CONFIG_EMMC_DDR52_EN 1
-	#define CONFIG_EMMC_DDR52_CLK 35000000
+
+#define CONFIG_AML_SD_EMMC			1
+#ifdef CONFIG_AML_SD_EMMC
+#define CONFIG_GENERIC_MMC
+#define CONFIG_CMD_MMC
+#define CONFIG_SYS_MMC_ENV_DEV			1
+#define CONFIG_EMMC_DDR52_EN			1
+#define CONFIG_EMMC_DDR52_CLK			35000000
 #endif
 
-#define	CONFIG_PARTITIONS 1
-#define CONFIG_EFI_PARTITION 1
-#define CONFIG_SYS_NO_FLASH  1
+#define CONFIG_SYS_NO_FLASH
 
-/* vpu */
-#define CONFIG_AML_VPU 1
+/* VPU */
+#define CONFIG_AML_VPU
 #ifdef CONFIG_AML_VPU
-#define CONFIG_VPU_PRESET 1
+#define CONFIG_VPU_PRESET
 #endif
 
-/* DISPLAY & HDMITX */
-#define CONFIG_AML_HDMITX20 1
-#define CONFIG_AML_CANVAS 1
-#define CONFIG_AML_VOUT 1
-#define CONFIG_AML_OSD 1
-#define CONFIG_OSD_SCALE_ENABLE 1
-#define CONFIG_CMD_BMP 1
+/* Display & HDMITX */
+#define CONFIG_AML_HDMITX20
+#define CONFIG_AML_CANVAS
+#define CONFIG_AML_VOUT
+#define CONFIG_AML_OSD
+#define CONFIG_OSD_SCALE_ENABLE
+#define CONFIG_CMD_BMP
 
 #if defined(CONFIG_AML_VOUT)
-#define CONFIG_AML_CVBS 1
+#define CONFIG_AML_CVBS
 #endif
 
 /* USB
  * Enable CONFIG_MUSB_HCD for Host functionalities MSC, keyboard
  * Enable CONFIG_MUSB_UDD for Device functionalities.
  */
-/* #define CONFIG_MUSB_UDC		1 */
-#define CONFIG_CMD_USB 1
+#define CONFIG_CMD_USB
 #if defined(CONFIG_CMD_USB)
-	#define CONFIG_GXL_XHCI_BASE		0xc9000000
-	#define CONFIG_GXL_USB_PHY2_BASE	0xd0078000
-	#define CONFIG_GXL_USB_PHY3_BASE	0xd0078080
-	#define CONFIG_USB_STORAGE      1
-	#define CONFIG_USB_XHCI		1
-	#define CONFIG_USB_XHCI_AMLOGIC_GXL 1
-#endif //#if defined(CONFIG_CMD_USB)
-
-//UBOOT Facotry usb/sdcard burning config
-#define CONFIG_AML_V2_FACTORY_BURN              1       //support facotry usb burning
-#define CONFIG_AML_FACTORY_BURN_LOCAL_UPGRADE   1       //support factory sdcard burning
-#define CONFIG_POWER_KEY_NOT_SUPPORTED_FOR_BURN 1       //There isn't power-key for factory sdcard burning
-#define CONFIG_SD_BURNING_SUPPORT_UI            1       //Displaying upgrading progress bar when sdcard/udisk burning
-
-#define CONFIG_AML_SECURITY_KEY                 1
-#define CONFIG_UNIFY_KEY_MANAGE                 1
-
-/* net */
-#define CONFIG_CMD_NET   1
-#if defined(CONFIG_CMD_NET)
-	#define CONFIG_DESIGNWARE_ETH 1
-	#define CONFIG_PHYLIB	1
-	#define CONFIG_NET_MULTI 1
-	#define CONFIG_CMD_PING 1
-	#define CONFIG_CMD_DHCP 1
-	#define CONFIG_CMD_RARP 1
-	#define CONFIG_HOSTNAME        arm_gxbb
-	#define CONFIG_ETHADDR         00:15:18:01:81:31   /* Ethernet address */
-	#define CONFIG_IPADDR          10.18.9.97          /* Our ip address */
-	#define CONFIG_GATEWAYIP       10.18.9.1           /* Our getway ip address */
-	#define CONFIG_SERVERIP        10.18.9.113         /* Tftp server ip address */
-	#define CONFIG_NETMASK         255.255.255.0
-#endif /* (CONFIG_CMD_NET) */
-
-/* other devices */
-#define CONFIG_EFUSE 1
-#define CONFIG_SYS_I2C_AML 1
-#define CONFIG_SYS_I2C_SPEED     400000
-
-/* commands */
-#define CONFIG_CMD_CACHE 1
-#define CONFIG_CMD_BOOTI 1
-#define CONFIG_CMD_EFUSE 1
-#define CONFIG_CMD_I2C 1
-#define CONFIG_CMD_MEMORY 1
-#define CONFIG_CMD_FAT 1
-#define CONFIG_CMD_EXT4 1
-#define CONFIG_CMD_GPIO 1
-#define CONFIG_CMD_RUN
-#define CONFIG_CMD_REBOOT 1
-#define CONFIG_CMD_ECHO 1
-#define CONFIG_CMD_JTAG	1
-#define CONFIG_CMD_AUTOSCRIPT 1
-#define CONFIG_CMD_MISC 1
-#define CONFIG_CMD_FS_UUID 1
-
-/*file system*/
-#define CONFIG_DOS_PARTITION 1
-#define CONFIG_MMC 1
-#define CONFIG_FS_FAT 1
-#define CONFIG_FS_EXT4 1
-#define CONFIG_LZO 1
-
-/* Cache Definitions */
-//#define CONFIG_SYS_DCACHE_OFF
-//#define CONFIG_SYS_ICACHE_OFF
-
-/* other functions */
-#define CONFIG_NEED_BL301	1
-#define CONFIG_NEED_BL32	1
-//#define CONFIG_CMD_RSVMEM	1
-#define CONFIG_FIP_IMG_SUPPORT	1
-#define CONFIG_BOOTDELAY	1 //delay 1s
-#define CONFIG_SYS_LONGHELP 1
-#define CONFIG_CMD_MISC     1
-#define CONFIG_CMD_ITEST    1
-#define CONFIG_CMD_CPU_TEMP 1
-#define CONFIG_SYS_MEM_TOP_HIDE 0x08000000 //hide 128MB for kernel reserve
-#define CONFIG_MULTI_DTB	1
-
-/* debug mode defines */
-//#define CONFIG_DEBUG_MODE           1
-#ifdef CONFIG_DEBUG_MODE
-#define CONFIG_DDR_CLK_DEBUG        636
-#define CONFIG_CPU_CLK_DEBUG        600
+#define CONFIG_GXL_XHCI_BASE			0xc9000000
+#define CONFIG_GXL_USB_PHY2_BASE		0xd0078000
+#define CONFIG_GXL_USB_PHY3_BASE		0xd0078080
+#define CONFIG_USB_STORAGE
+#define CONFIG_USB_XHCI
+#define CONFIG_USB_XHCI_AMLOGIC_GXL
 #endif
 
-//support secure boot
-#define CONFIG_AML_SECURE_UBOOT   1
+/* U-Boot Factory USB/SD burning config */
+#define CONFIG_AML_V2_FACTORY_BURN
+#define CONFIG_AML_FACTORY_BURN_LOCAL_UPGRADE
+#define CONFIG_POWER_KEY_NOT_SUPPORTED_FOR_BURN
+#define CONFIG_SD_BURNING_SUPPORT_UI		1
 
+#define CONFIG_AML_SECURITY_KEY
+#define CONFIG_UNIFY_KEY_MANAGE
+
+/* NET */
+#define CONFIG_CMD_NET
+#if defined(CONFIG_CMD_NET)
+#define CONFIG_DESIGNWARE_ETH
+#define CONFIG_PHYLIB
+#define CONFIG_NET_MULTI
+#define CONFIG_CMD_PING
+#define CONFIG_CMD_DHCP
+#define CONFIG_CMD_RARP
+#define CONFIG_HOSTNAME				arm_gxbb
+#define CONFIG_ETHADDR				00:15:18:01:81:31	/* Ethernet address */
+#define CONFIG_IPADDR				10.18.9.97		/* Our ip address */
+#define CONFIG_GATEWAYIP			10.18.9.1		/* Our getway ip address */
+#define CONFIG_SERVERIP				10.18.9.113		/* Tftp server ip address */
+#define CONFIG_NETMASK				255.255.255.0
+#endif
+
+/* other devices */
+#define CONFIG_EFUSE
+#define CONFIG_SYS_I2C_AML
+#define CONFIG_SYS_I2C_SPEED			400000
+
+/* commands */
+#define CONFIG_CMD_CACHE
+#define CONFIG_CMD_BOOTI
+#define CONFIG_CMD_EFUSE
+#define CONFIG_CMD_I2C
+#define CONFIG_CMD_MEMORY
+#define CONFIG_CMD_FAT
+#define CONFIG_CMD_EXT4
+#define CONFIG_CMD_GPIO
+#define CONFIG_CMD_RUN
+#define CONFIG_CMD_REBOOT
+#define CONFIG_CMD_ECHO
+#define CONFIG_CMD_JTAG
+#define CONFIG_CMD_AUTOSCRIPT
+#define CONFIG_CMD_MISC
+#define CONFIG_CMD_FS_UUID
+
+/* file systems */
+#define CONFIG_DOS_PARTITION
+#define CONFIG_EFI_PARTITION
+#define CONFIG_MMC
+#define CONFIG_FS_FAT
+#define CONFIG_FS_EXT4
+#define CONFIG_LZO
+
+/* other functions */
+#define CONFIG_NEED_BL301
+#define CONFIG_NEED_BL32
+#define CONFIG_FIP_IMG_SUPPORT
+#define CONFIG_BOOTDELAY			1
+#define CONFIG_SYS_LONGHELP
+#define CONFIG_CMD_MISC
+#define CONFIG_CMD_ITEST
+#define CONFIG_CMD_CPU_TEMP
+#define CONFIG_SYS_MEM_TOP_HIDE			0x08000000	/* hide 128MB for kernel reserve */
+#define CONFIG_MULTI_DTB
+
+/* debug mode defines */
+#ifdef CONFIG_DEBUG_MODE
+#define CONFIG_DDR_CLK_DEBUG			636
+#define CONFIG_CPU_CLK_DEBUG			600
+#endif
+
+/* support secure boot */
+#define CONFIG_AML_SECURE_UBOOT
 #if defined(CONFIG_AML_SECURE_UBOOT)
+#define CONFIG_AML_CRYPTO_UBOOT
+#endif
 
-//for SRAM size limitation just disable NAND
-//as the socket board default has no NAND
-//#undef CONFIG_AML_NAND
-
-//unify build for generate encrypted bootloader "u-boot.bin.encrypt"
-#define CONFIG_AML_CRYPTO_UBOOT   1
-
-//unify build for generate encrypted kernel image
-//SRC : "board/amlogic/(board)/boot.img"
-//DST : "fip/boot.img.encrypt"
-//#define CONFIG_AML_CRYPTO_IMG       1
-
-#endif //CONFIG_AML_SECURE_UBOOT
-
-#define CONFIG_SECURE_STORAGE 1
-
-//build with uboot auto test
-//#define CONFIG_AML_UBOOT_AUTO_TEST 1
-
-//board customer ID
-//#define CONFIG_CUSTOMER_ID  (0x6472616F624C4D41)
-
+#define CONFIG_SECURE_STORAGE
 #define CONFIG_SUPPORT_RAW_INITRD
 
 #if defined(CONFIG_CUSTOMER_ID)
-  #undef CONFIG_AML_CUSTOMER_ID
-  #define CONFIG_AML_CUSTOMER_ID  CONFIG_CUSTOMER_ID
+#undef CONFIG_AML_CUSTOMER_ID
+#define CONFIG_AML_CUSTOMER_ID			CONFIG_CUSTOMER_ID
 #endif
+
 #define ETHERNET_INTERNAL_PHY
 
 #endif
-
